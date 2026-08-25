70dba656e56df060ee9b94db9c4cea1a6df0bf85
Alex Nodeland <nodeland.alex@gmail.com>
2026-08-25 13:37:40 -0400

    whisk: v0.4.0 -> v0.4.1

--- Casks/whisk.rb
+++ Casks/whisk.rb
@@ -1,6 +1,6 @@
 cask "whisk" do
-  version "0.4.0"
-  sha256 "e7a473e6a6c61a00c52d3b048ba239b87a1b3d7f7f07391f5f25b375767c765f"
+  version "0.4.1"
+  sha256 "33ce348dd2464d0c89fadf66adb94cf0f81b9de7329fc9d392bbb01643cf719f"
 
   url "https://github.com/alexnodeland/whisk/releases/download/v#{version}/Whisk-universal.zip"
   name "Whisk"
